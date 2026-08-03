592311c07d3d265a3409a0ca3c23b4d66062062c
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-08-03 15:57:57 -0400

    feat(ms-identidad-acceso): scaffolding inicial - Config Client, Eureka Client, JPA, Flyway, Actuator

---
+++ ms-identidad-acceso/src/main/java/cl/sige/plataforma/ms_identidad_acceso/MsIdentidadAccesoApplication.java
@@ -0,0 +1,13 @@
+package cl.sige.plataforma.ms_identidad_acceso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIdentidadAccesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsIdentidadAccesoApplication.class, args);
+	}
+
+}

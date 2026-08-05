a21dc7f37a9b0c64a9d450413b85854d89173af7
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-08-04 21:10:00 -0400

    chore(ms-identidad-acceso): habilitar ConfigurationPropertiesScan y eliminar VerificacionInicialRunner

--- ms-identidad-acceso/src/main/java/cl/sige/plataforma/ms_identidad_acceso/MsIdentidadAccesoApplication.java
+++ ms-identidad-acceso/src/main/java/cl/sige/plataforma/ms_identidad_acceso/MsIdentidadAccesoApplication.java
@@ -2,8 +2,10 @@ package cl.sige.plataforma.ms_identidad_acceso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MsIdentidadAccesoApplication {
 
 	public static void main(String[] args) {

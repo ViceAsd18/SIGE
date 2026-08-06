870894a95c7675ab0360fc79b6e5af3d0c427770
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-08-06 17:11:00 -0400

    feat(ms-estudiantes): cliente Feign IdentidadAccesoClient para validar PersonaRol

---
+++ ms-estudiantes/src/main/java/cl/sige/plataforma/ms_estudiantes/client/IdentidadAccesoClient.java
@@ -0,0 +1,13 @@
+package cl.sige.plataforma.ms_estudiantes.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import cl.sige.plataforma.ms_estudiantes.client.dto.PersonaRolClientResponse;
+
+@FeignClient(name = "ms-identidad-acceso")
+public interface IdentidadAccesoClient {
+
+    @GetMapping("/persona-roles/{id}")
+    PersonaRolClientResponse obtenerPersonaRol(@PathVariable("id") Long id);
+}

d6260463ddda3601dd2086b04ebfd9e65bac168c
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-08-07 22:52:59 -0400

    feat(ms-academico): scaffolding inicial - Config Client, Eureka Client, JPA, Flyway, Actuator, Lombok

---
+++ ms-academico/src/main/java/cl/sige/plataforma/ms_academico/MsAcademicoApplication.java
@@ -0,0 +1,13 @@
+package cl.sige.plataforma.ms_academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAcademicoApplication.class, args);
+	}
+
+}
---
+++ ms-academico/src/test/java/cl/sige/plataforma/ms_academico/MsAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sige.plataforma.ms_academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

641f8fa3b1459768941132bbfeea11a2ca80034f
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-08-17 14:31:21 -0400

    feat(ms-academico): agregar OpenFeign y habilitar EnableFeignClients

--- ms-academico/src/main/java/cl/sige/plataforma/ms_academico/MsAcademicoApplication.java
+++ ms-academico/src/main/java/cl/sige/plataforma/ms_academico/MsAcademicoApplication.java
@@ -2,8 +2,12 @@ package cl.sige.plataforma.ms_academico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@ConfigurationPropertiesScan
 public class MsAcademicoApplication {
 
 	public static void main(String[] args) {

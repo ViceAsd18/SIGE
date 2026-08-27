7518f4c24c82b5919cc7faa9de3a047b485d17c2
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-08-26 22:01:02 -0400

    feat(ms-comunicaciones): excepciones de dominio y GlobalExceptionHandler

---
+++ ms-comunicaciones/src/main/java/cl/sige/plataforma/ms_comunicaciones/exception/RecursoInvalidoException.java
@@ -0,0 +1,5 @@
+package cl.sige.plataforma.ms_comunicaciones.exception;
+
+public class RecursoInvalidoException extends RuntimeException {
+    public RecursoInvalidoException(String mensaje) { super(mensaje); }
+}

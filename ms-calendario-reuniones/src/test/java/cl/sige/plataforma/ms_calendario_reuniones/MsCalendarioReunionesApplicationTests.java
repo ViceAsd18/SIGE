efc2a1abce813f6e66970f1c1f8e56c7b9fb3976
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-09-15 20:39:08 -0300

    fix(ms-calendario-reuniones): migrar de Gradle a Maven - el proyecto nunca tuvo pom.xml comiteado

---
+++ ms-calendario-reuniones/src/test/java/cl/sige/plataforma/ms_calendario_reuniones/MsCalendarioReunionesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sige.plataforma.ms_calendario_reuniones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCalendarioReunionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
